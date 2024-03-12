45bb99a79c692414bee9e8556c809d407ce27b3f
Nikita Stavtsev <kwzee@yandex.ru>
2024-03-12 17:14:33 +0300

    [emacs] update

--- emacs/init.el
+++ emacs/init.el
@@ -32,6 +32,57 @@
   :init
   (evil-mode 1))
 
+;; -= which-key =-
+(use-package which-key
+  :straight t
+  :config
+  (which-key-mode 1))
+
+;; -= counsel + ivy + swiper =-
+(use-package counsel
+  :straight t
+  :config
+  (counsel-mode 1)
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t))
+
+(global-set-key "\C-s" 'swiper)
+
+;; -= treemacs =-
+(use-package treemacs
+  :straight t)
+
 ;; -= magit =-
 (use-package magit
+  :straight t)
+
+;; -= projectile =-
+(use-package projectile
+  :straight t
+  :init
+  (projectile-mode 1)
+  :bind (:map projectile-mode-map
+	      ("C-c p" . projectile-command-map)))
+
+;; -= lsp =-
+(use-package lsp-mode
+  :straight t
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :hook
+  (c++-mode . lsp)
+  (lsp-mode . lsp-enable-which-key-integration))
+
+(use-package lsp-ui
+  :straight t)
+
+(use-package lsp-ivy
+  :straight t)
+
+(use-package lsp-treemacs
+  :straight t)
+
+;; -= dap =-
+(use-package dap-mode
   :straight t)
